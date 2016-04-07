b8d81397fbd5491dce40545d8f06e48ab9b42ae8
shaohaiyan <shaohaiyan2015@outlook.com>
2016-04-07 10:48:38 +0800

    测试

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * <p>Description：</p>
+ * <p>Copyright：Copyright (c) 2016</p>
+ * <p>Company：SenseTime</p>
+ * <p>Email：shaohaiyan@sensetime.com</p>
+ *
+ * @author shaohaiyan
+ * @date 16-4-7:上午10:47
+ */
+public class Test {
+
+}
