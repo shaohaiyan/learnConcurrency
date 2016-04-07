543c0cd109db1589f8ef1753c872c6ec634b7fcc
shaohaiyan <shaohaiyan2015@outlook.com>
2016-04-07 18:00:39 +0800

    sdff

---
+++ src/main/java/com/sensetime/thinkJava/exercise/Exercise22.java
@@ -0,0 +1,13 @@
+package com.sensetime.thinkJava.exercise;
+
+/**
+ * <p>Description：</p>
+ * <p>Copyright：Copyright (c) 2016</p>
+ * <p>Company：SenseTime</p>
+ * <p>Email：shaohaiyan@sensetime.com</p>
+ *
+ * @author shaohaiyan
+ * @date 16-4-7:下午5:37
+ */
+public class Exercise22 {
+}
