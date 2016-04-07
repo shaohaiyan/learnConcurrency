61583f20983b7d2c894afe05328d7dfa25b11c34
shaohaiyan <shaohaiyan2015@outlook.com>
2016-04-07 18:01:09 +0800

    sdff

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,5 +8,27 @@
  * @date 16-4-7:上午10:47
  */
 public class Test {
+    public static void main(String[] args) {
+        Thread.interrupted();
 
+        /**
+         * waxon:
+         *  sleep模拟打蜡；
+         *  waxed();标识已经打蜡完毕，准备抛光
+         *      waxon=true;
+         *      notifyAll();
+         *  waitingForBuffing
+         *      while(wax==true) wait();只要不进行抛光或者抛光完毕那么就一直等待
+         *
+         *  waxoff:
+         *   waitFOrWaxing()
+         *      while(wax==false) wait();只要打蜡还在进行那么这个线程就等待
+         *      sleep；模拟抛光
+         *      buffed（）；抛光完毕
+         *          waxon=fasle；抛光完毕准备下一轮的打蜡，当waxon为true时，waitForBuffer会一直进行，只有设置为false，waxon线程才不会等待
+         *          notifyAll（）；唤醒等待的waxon线程
+         *
+         *
+         */
+    }
 }
--- src/main/java/com/sensetime/thinkJava/exercise/Exercise21.java
+++ src/main/java/com/sensetime/thinkJava/exercise/Exercise21.java
@@ -1,5 +1,9 @@
 package com.sensetime.thinkJava.exercise;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  * <p>Description：</p>
  * <p>Copyright：Copyright (c) 2016</p>
@@ -10,4 +14,56 @@ package com.sensetime.thinkJava.exercise;
  * @date 16-4-7:上午11:31
  */
 public class Exercise21 {
+public static void main(String [] args){
+    ExecutorService executorService= Executors.newCachedThreadPool();
+    Runnable task1=new Task1();
+    Runnable task2=new Task2(task1);
+    executorService.execute(task1);
+    executorService.execute(task2);
+    Thread.yield();
+    executorService.shutdown();
+}
+
+
+}
+
+//一开始就wait
+class Task1 implements Runnable{
+
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            try {
+                wait();
+                System.out.println("离开letWait（）");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
+class Task2 implements Runnable{
+    private Runnable  runnable;
+    public Task2(Runnable runnable){
+        this.runnable=runnable;
+    }
+    @Override
+    public void run() {
+        try {
+            System.out.println("Task2开始执行");
+            TimeUnit.SECONDS.sleep(1);
+            System.out.println("开始唤醒  "+runnable);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        synchronized (runnable){
+            runnable.notifyAll();
+        }
+    }
 }
